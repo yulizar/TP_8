73ae87ab09d25b8af4c26d0b6ccad023411eb11d
rosekumala <radenroro.rk@gmail.com>
2016-11-06 11:07:02 +0700

    Update Aplikasi.java

--- src/model/Aplikasi.java
+++ src/model/Aplikasi.java
@@ -17,7 +17,29 @@ public class Aplikasi {
     private ArrayList<Mobil> listMobil;
     private FileIO database;
     
-    public void createAkun(){
-        
+    void createAkun(Member member){
+        listMember.add(member);
+    }
+    void addKategori(Kategori kategori){
+        listKategori.add(kategori);
+    }
+    void addMobil(Mobil mobil){
+        listMobil.add(mobil);
+    }
+    void loadMobil() throws IOException, ClassNotFoundException{
+            listMobil = (ArrayList<Mobil>) database.loadObject("mobil.dat");
+    }
+    void saveMobil() throws IOException{
+        for (int i = 0; i<listMobil.size(); i++){
+            database.saveObject(listMobil.get(i), "mobil.dat");
+        }
+    }
+    void loadKategori() throws IOException, ClassNotFoundException{
+            listKategori = (ArrayList<Kategori>) database.loadObject("kategori.dat");
+    }
+    void saveKategori() throws IOException{
+        for (int i = 0; i<listKategori.size(); i++){
+            database.saveObject(listKategori.get(i), "kategori.dat");
+        }
     }
 }
